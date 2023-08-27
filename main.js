959ef173bd3e0a453c5d4a04f82ec5e6831e9176
Willian Papa <willianpapa@yahoo.com.br>
2023-08-27 13:34:48 -0300

    valor

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const form = document.getElementById('form-deposito');
 
+function validaValor(valorMensalidade) {
+    const valorComoArray = valorMensalidade == 300.00;
+    return valorComoArray == 300.00;
+}
+
 form.addEventListener('submit', function(e) {
     e.preventDefault()
 })
