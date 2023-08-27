0edb5b95ed7fb57878c66ea0401454d4f9981831
Willian Papa <willianpapa@yahoo.com.br>
2023-08-27 13:45:43 -0300

    elso

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('form-deposito');
 
 function validaValor(valorMensalidade) {
-    const valorComoArray = valorMensalidade == 300.00;
-    return valorComoArray == 300.00;
+    const valorComoArray = valorMensalidade == 300;
+    return valorComoArray == 300;
 }
 
 form.addEventListener('submit', function(e) {
@@ -10,7 +10,9 @@ form.addEventListener('submit', function(e) {
 
     const valorMensalidade = document.getElementById('valor-mensalidade');
     if (!validaValor(valorMensalidade.value)) {
-        alert("Valor correto 300.00")
+        alert("Valor correto 300");
+    } else {
+        alert("Tudo certo!");
     }
 })
 
